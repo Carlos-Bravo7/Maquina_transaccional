7ce1643a47382dfebbb0f2d966096fcb6199540a
Carlos-Bravo7 <cabpdutydvlopmnt@gmail.com>
2023-04-30 13:12:02 -0500

    creacion vista consultar saldo

---
+++ maquina_transaccional/lib/views/consultar_saldo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class consultar_saldo extends StatelessWidget {
+  const consultar_saldo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
