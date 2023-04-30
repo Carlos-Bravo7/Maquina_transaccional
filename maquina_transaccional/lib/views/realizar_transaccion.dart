38dd1761b0672ca647571e4842f67e7d452a728f
Carlos-Bravo7 <cabpdutydvlopmnt@gmail.com>
2023-04-30 13:15:16 -0500

    creacion vista realizar transaccion

---
+++ maquina_transaccional/lib/views/realizar_transaccion.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class realizar_transaccion extends StatelessWidget {
+  const realizar_transaccion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
